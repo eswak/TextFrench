001ad3e5c7947b665d5412f0ef0f874d503ca42b
jfdesrochers <jfdesrochers@users.noreply.github.com>
2015-04-17 15:25:18 -0400

    Completed Translation

---
+++ src/num2words-en.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "string.h"
+
+void time_to_words(int hours, int minutes, char* words, size_t length);
+void time_to_words2(int hours, int minutes, char* words, size_t length);
+void time_to_3words(int hours, int minutes, char *line1, char *line2, char *line3, size_t length);
+void time_to_4words(int hours, int minutes, char *line1, char *line2, char *line3, char *line4, size_t length);
